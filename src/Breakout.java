8074c5920c088235f73f7f8289a4b1f4c0ab1edd
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-05-08 12:03:55 +0100

    Created JFrame and properties

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,4 +1,15 @@
+import javax.swing.*;
+
 public class Breakout {
 
-d
+    public static void main (String [] args){
+        JFrame obj = new JFrame();
+        obj.setBounds(10,10, 700,600);
+        obj.setTitle("Breakout");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
 }
