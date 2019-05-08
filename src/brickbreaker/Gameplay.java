782dc4f6f93b6471f4ac510b761ac515c9419b3d
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-05-08 13:41:10 +0100

    Added game properties

--- src/brickbreaker/Gameplay.java
+++ src/brickbreaker/Gameplay.java
@@ -10,7 +10,24 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private boolean play = false; // Prevents game from starting by itself
     private int score = 0; //The starting off score
     private int totalbrick = 21; //  Number of bricks in game
-    private Timer time; // how fast the ball moves 
+    private Timer time; // how fast the ball moves
+    private  int delay = 8; // the speed for timer
+
+    private  int playerX = 310; // Starting location of paddle
+
+    private int ballposX = 120; // position of ball on the X axis
+    private int ballposY = 350; // position of ball on teh Y axis
+    private int ballXdir = -1; // sets the diagonal direction of the ball x axis
+    private int ballYdir = -2; // sets the diagonal direction of teh ball y axis
+
+
+    public Gameplay(){
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        time = new Timer(delay, this);
+        time.start();
+    }
 
 
     @Override
